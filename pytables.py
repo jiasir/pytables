a901f9bdaaeb7ff0209a1b7e5c6978593aaff4e2
jiasir <jiasir@icloud.com>
2015-04-06 16:44:32 +0800

    Add logging feature

--- pytables.py
+++ pytables.py
@@ -2,6 +2,7 @@
 __author__ = 'jiasir'
 import pika
 import subprocess
+import logging
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.20.10'))
@@ -11,6 +12,10 @@ channel.exchange_declare(exchange='ip_exchange', type='fanout')
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
 channel.queue_bind(exchange='ip_exchange', queue=queue_name)
+
+logger = logging.getLogger('pytables')
+logging.basicConfig(filename='/var/log/pytables.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 print ' [*] Waiting for ip. To exit press CTRL+C'
 
@@ -26,7 +31,7 @@ def callback(ch, method, properties, body):
     if body not in d:
         deny_ip(body)
         d[body] = 1
-        print " [x] Drop: %r" % (body,)
+        logger.info(" [x] Drop: %r" % (body,))
 
 
 channel.basic_consume(callback, queue=queue_name, no_ack=True)
